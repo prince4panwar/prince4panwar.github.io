b7caa8adce0e5f74fa208f5e42ca7cfcf749eba4
Prince Panwar <130494609+prince4panwar@users.noreply.github.com>
2024-02-26 16:38:23 +0530

    undo animation

--- script.js
+++ script.js
@@ -94,41 +94,6 @@ window.addEventListener("scroll", scrollActive);
 // sr.reveal(".home__social-icon", { interval: 200 });
 // sr.reveal(".skills__data, .work__img, .contact__input", { interval: 200 });
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-  origin: "top",
-  distance: "60px",
-  duration: 2000,
-  delay: 200,
-  //     reset: true
-});
-
-sr.reveal(".home__data, .about__img, .skills__subtitle, .skills__text ", {});
-sr.reveal(".home__img, .about__subtitle, .about__text, .skills__img", {
-  delay: 400,
-});
-sr.reveal(".nav__item, .home__social-icon, .skill-boxes", {
-  interval: 200,
-});
-
-sr.reveal(".skills__data, .work__img, .contact__input, .footer__title", {
-  interval: 200,
-});
-
-ScrollReveal().reveal(".button,  .footer__social i", {
-  delay: 200,
-  distance: "-100px",
-  duration: 1000,
-  // easing: "ease-in-out",
-  origin: "left",
-  delay: 200,
-  interval: 200,
-  // reset: true,
-});
-
-
-
-
 
 // function toggleDarkMode() {
 //   var element = document.body;
